27e1aca043bae522e5880003a2a46f3480b6fb87
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-03 15:58:33 -0700

    chore: style fixes for hamburger nav menu

--- app/_components/HeaderNav.tsx
+++ app/_components/HeaderNav.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -17,7 +18,7 @@ export default function HeaderNav() {
         <HamburgerNavButton />
         <Link href="/">
           <div className="row items-center p-2 text-white h-[42px]">
-            <Image
+            <img
               src={`/artbot-logo.png`}
               height={30}
               width={30}
