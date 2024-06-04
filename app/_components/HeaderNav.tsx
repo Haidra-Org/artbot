e98055b3e68210bdf5fc3b891040e76c223d7882
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-03 20:25:54 -0700

    chore: remove unused import

--- app/_components/HeaderNav.tsx
+++ app/_components/HeaderNav.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
-import Image from 'next/image'
 import Link from 'next/link'
 import HamburgerNavButton from './HamburgerNavButton'
 import UserKudos from './HeaderNavUserKudos'
