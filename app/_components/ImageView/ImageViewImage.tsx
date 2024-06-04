50af83744339104396ae3d46705f74e40a10d0b7
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-03 23:26:49 -0700

    feat: ImageView component to view images in an image viewer
    chore: More Image View stuff

---
+++ app/_components/ImageView/ImageViewImage.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from 'react'
+import { useImageView } from './ImageViewProvider'
+import Carousel from '../Carousel'
+
+const defaultImage =
+  'data:image/gif;base64,R0lGODdhAQABAJEAAAAAAB8fH////wAAACH5BAkAAAMALAAAAAABAAEAAAICTAEAOw=='
+
+// Prevents re-rendering of the same image multiple times as parent is updated
+const ImageViewImage = React.memo(() => {
+  const { artbot_id, imageBlob, imageData } = useImageView()
+  const { imageFiles } = imageData
+
+  const [imageUrl, setImageUrl] = useState(defaultImage)
+
+  useEffect(() => {
+    let isMounted = true // To prevent state update on unmounted component
+
+    const fetchImage = async () => {
+      if (imageBlob && isMounted) {
+        const url = URL.createObjectURL(imageBlob)
+        setImageUrl(url)
+      }
+    }
+
+    fetchImage()
+
+    return () => {
+      isMounted = false // Clean up to prevent memory leaks
+
+      if (imageUrl !== defaultImage) {
+        URL.revokeObjectURL(imageUrl) // Clean up blob URL
+      }
+    }
+
+    // imageUrl in this dep array causes issue with URL.createObjectURL re-rendering thousands of times.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [artbot_id, imageBlob])
+
+  if (!imageBlob) return null
+
+  return <Carousel slides={imageFiles} options={{ loop: true }} />
+})
+
+ImageViewImage.displayName = 'ImageViewImage'
+export default ImageViewImage
---
+++ app/_components/ImageView/ImageViewInfoContainer.tsx
@@ -0,0 +1,52 @@
+import {
+  IconPlaylistAdd,
+  IconPlaylistX,
+  IconSettings
+} from '@tabler/icons-react'
+import ImageViewActions from './imageViewActions'
+import styles from './imageView.module.css'
+
+import { useImageView } from './ImageViewProvider'
+import ImageDetails from '../ImageDetails'
+
+export default function ImageViewInfoContainer({
+  onDelete
+}: {
+  onDelete: () => void
+}) {
+  const { imageData, imageId } = useImageView()
+  const { imageRequest } = imageData
+
+  return (
+    <div className={styles.ImageInfoContainer}>
+      <ImageViewActions onDelete={onDelete} />
+      <div className="col gap-1 w-full">
+        <div className="row gap-2 text-sm font-bold">
+          <IconPlaylistAdd stroke={1} />
+          Prompt
+        </div>
+        <div className="w-full text-sm ml-[8px] break-words">
+          {imageRequest?.prompt}
+        </div>
+      </div>
+      {imageRequest?.negative && (
+        <div className="col gap-0 w-full">
+          <div className="row gap-2 text-sm font-bold">
+            <IconPlaylistX stroke={1} />
+            Negative
+          </div>
+          <div className="w-full text-sm ml-[8px] break-words">
+            {imageRequest?.negative}
+          </div>
+        </div>
+      )}
+      <div className="col gap-2 w-full">
+        <div className="row gap-2 text-sm font-bold">
+          <IconSettings stroke={1} />
+          Image details
+        </div>
+        <ImageDetails image_id={imageId as string} />
+      </div>
+    </div>
+  )
+}
