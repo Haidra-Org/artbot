29b6e3951d53a2ee2eeca74b9603cd758b72fab4
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-11 21:53:28 -0700

    chore: update Dexie to support AppSettingsTable

--- app/_types/ArtbotTypes.ts
+++ app/_types/ArtbotTypes.ts
@@ -7,6 +7,12 @@ export interface AiHordeEmbedding extends Embedding {
   inject_ti?: InjectTi
 }
 
+// Holds various types of settings that may be used throughout the web app.
+export interface AppSettingsTable {
+  key: string // indexed for quick lookups
+  value: unknown
+}
+
 // Simplified CivitAi types that ArtBot will use to cast API requests to the proper type
 export type CivitAiBaseModels = 'SDXL' | 'Pony' | 'SD 1.x' | 'SD 2.x' | 'NSFW'
 
