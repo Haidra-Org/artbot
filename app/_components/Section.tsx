a3daefc8d28a82a6e2ba0ab60506aa2d923ec1d4
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-27 06:56:03 -0700

    fix: style issues with section anchor

--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -27,13 +27,16 @@ export default function Section({
             backgroundColor: 'gray',
             borderTopLeftRadius: '0.375rem',
             borderBottomLeftRadius: '0.375rem',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
             height: '100%',
             width: '8px'
           }}
           href={`#${anchor}`}
         ></a>
       )}
-      <div className="col px-2 py-2 gap-2 w-full">
+      <div className={clsx('col px-2 py-2 gap-2 w-full', anchor ? 'pl-5' : '')}>
         {title && <h2 className="row font-bold text-white">{title}</h2>}
         {children}
       </div>
