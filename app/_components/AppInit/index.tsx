3dc580efa32a72dacee3852882660be17d1a27d2
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-06-26 23:10:24 -0700

    feat: fetch and store available model data on page load

--- app/_components/AppInit/index.tsx
+++ app/_components/AppInit/index.tsx
@@ -1,42 +1,70 @@
+import { clientHeader } from '@/app/_data-models/ClientHeader'
 import AppInitComponent from './AppInitComponent'
+import { AvailableImageModel } from '@/app/_types/HordeTypes'
 
 export async function getData() {
   try {
-    const res = await fetch(
-      `https://raw.githubusercontent.com/Haidra-Org/AI-Horde-image-model-reference/main/stable_diffusion.json`,
-      {
+    const availableUrl = `https://aihorde.net/api/v2/status/models`
+    const detailsUrl = `https://raw.githubusercontent.com/Haidra-Org/AI-Horde-image-model-reference/main/stable_diffusion.json`
+
+    const [availableRes, detailsRes] = await Promise.allSettled([
+      fetch(availableUrl, {
+        headers: {
+          'Client-Agent': clientHeader(),
+          'Content-Type': 'application/json'
+        },
+        next: { revalidate: 120 } // Revalidate every 120 seconds
+      }),
+      fetch(detailsUrl, {
         cache: 'no-store',
         headers: {
           'Content-Type': 'application/json'
         },
         method: 'GET'
-      }
-    )
+      })
+    ])
 
-    const data = await res.json()
+    const availableData =
+      availableRes.status === 'fulfilled' ? await availableRes.value.json() : []
+    const detailsData =
+      detailsRes.status === 'fulfilled' ? await detailsRes.value.json() : {}
+
+    const availableFiltered = availableData
+      .filter((model: AvailableImageModel) => model.type === 'image')
+      .sort(
+        (a: AvailableImageModel, b: AvailableImageModel) => b.count - a.count
+      )
 
     // Parse json to ensure validity
-    const jsonString = JSON.stringify(data)
+    const jsonString = JSON.stringify(detailsData)
     const json = JSON.parse(jsonString)
 
     // Optional: Additional checks to ensure the JSON structure is as expected
     if (!json || typeof json !== 'object' || Object.keys(json).length === 0) {
       return {
+        modelsAvailable: [],
         modelDetails: {}
       }
     }
 
     return {
-      modelDetails: data
+      modelsAvailable: availableFiltered,
+      modelDetails: detailsData
     }
   } catch (error) {
     return {
+      modelsAvailable: [],
       modelDetails: {}
     }
   }
 }
 
 export default async function AppInit() {
-  const { modelDetails } = await getData()
-  return <AppInitComponent modelDetails={modelDetails} />
+  const { modelsAvailable, modelDetails } = await getData()
+  return (
+    <AppInitComponent
+      modelsAvailable={modelsAvailable}
+      modelDetails={modelDetails}
+    />
+  )
 }
