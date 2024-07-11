b1440470af68ac62a93bec6c694027840c1020f8
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-07-10 20:48:52 -0700

    chore: update metadate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Toaster } from 'react-hot-toast'
 
 import HeaderNav from './_components/HeaderNav'
@@ -9,10 +9,48 @@ import BetaWarningPanel from './_components/BetaWarningPanel'
 import MobileFooter from './_components/MobileFooter'
 import AppInit from './_components/AppInit'
 
+const APP_NAME = 'ArtBot'
+const APP_DEFAULT_TITLE = 'ArtBot for Stable Diffusion'
+const APP_TITLE_TEMPLATE = '%s | ArtBot'
+const APP_DESCRIPTION =
+  'Generate AI-created images and photos for free, with ArtBot, using an open source distributed computing cluster of Stable Diffusion GPUs powered by volunteers and the AI Horde. No login required and free to use.'
+
 export const metadata: Metadata = {
-  title: 'ArtBot for Stable Diffusion',
-  description:
-    'Generate AI-created images and photos with Stable Diffusion using an open source distributed computing cluster powered by the AI Horde. No login required and free to use.'
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+}
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF'
 }
 
 export default function RootLayout({
