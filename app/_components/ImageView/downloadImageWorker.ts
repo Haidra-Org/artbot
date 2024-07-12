ee5546f6ebfa5c3e8dc5fd4488e80694bf274c2e
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-07-11 23:39:53 -0700

    chore: initial work on metadata

--- app/_components/ImageView/downloadImageWorker.ts
+++ app/_components/ImageView/downloadImageWorker.ts
@@ -1,11 +1,16 @@
+import { PNG } from 'pngjs'
+import { Buffer } from 'buffer'
+import { addMetadata } from 'meta-png'
+
 type WorkerMessage = {
   imageBlob: Blob
+  comment: string
 }
 
 self.addEventListener('message', (e: MessageEvent<WorkerMessage>) => {
-  const { imageBlob } = e.data
+  const { imageBlob, comment } = e.data
 
-  convertAndAddMetadata(imageBlob)
+  convertAndAddMetadata(imageBlob, comment)
     .then((pngBlob) => {
       ;(self as unknown as DedicatedWorkerGlobalScope).postMessage({ pngBlob })
     })
@@ -17,12 +22,32 @@ self.addEventListener('message', (e: MessageEvent<WorkerMessage>) => {
     })
 })
 
-async function convertAndAddMetadata(imageBlob: Blob): Promise<Blob> {
+async function convertBlobToPngUint8Array(blob: Blob): Promise<Uint8Array> {
+  const arrayBuffer = await blob.arrayBuffer()
+
+  // Convert ArrayBuffer to Buffer
+  const buffer = Buffer.from(arrayBuffer)
+
+  // Use pngjs to read the PNG data
+  const png = PNG.sync.read(buffer)
+
+  // Convert the PNG data to a Uint8Array
+  const pngUint8Array = new Uint8Array(PNG.sync.write(png))
+
+  return pngUint8Array
+}
+
+async function convertAndAddMetadata(
+  imageBufferArray: Blob
+  // comment: string
+): Promise<Blob | undefined> {
   try {
-    return await convertBlob(imageBlob, 'image/png')
+    const imageBlob = await convertBlob(imageBufferArray, 'image/png')
+    const PNGUint8Array = await convertBlobToPngUint8Array(imageBlob)
+    const updated = addMetadata(PNGUint8Array, 'foo', 'bar')
+    return new Blob([updated], { type: 'image/png' })
   } catch (error) {
-    console.error('Error details:', error)
-    throw new Error('Error processing image: ' + (error as Error).message)
+    console.error('Error in convertAndAddMetadata:', error)
   }
 }
 
