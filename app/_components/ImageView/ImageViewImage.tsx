16a539e5ae005d81ce65246870d4eaff2bb57d5f
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-07-18 21:44:06 -0700

    chore: fix issue with image sizes in carousel

--- app/_components/ImageView/ImageViewImage.tsx
+++ app/_components/ImageView/ImageViewImage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useImageView } from './ImageViewProvider'
 import CarouselImage from '../Carousel/CarouselImage'
 import Carousel from '../Carousel'
@@ -7,14 +7,31 @@ import { ImageBlobBuffer } from '@/app/_data-models/ImageFile_Dexie'
 
 // Prevents re-rendering of the same image multiple times as parent is updated
 const ImageViewImage = React.memo(() => {
+  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight)
   const { imageBlobBuffer, imageData, setCurrentImageId } = useImageView()
   const { imageFiles } = imageData
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   if (!imageFiles) return null
   if (!imageBlobBuffer) return null
 
+  // Constrain max width of image so that multiple images don't show in carousel.
+  const imageWidth =
+    ((windowHeight - 72) * imageData?.imageRequest?.width) /
+    imageData?.imageRequest?.height
+
   return (
-    <div style={{ maxWidth: `${imageData?.imageRequest?.width}px` }}>
+    <div style={{ maxWidth: `${imageWidth}px` }}>
       <Carousel
         numSlides={imageFiles.length}
         onSlideChange={(num: number) => {
@@ -26,7 +43,7 @@ const ImageViewImage = React.memo(() => {
           <CarouselImage
             key={image.image_id}
             imageBlobBuffer={image.imageBlobBuffer as ImageBlobBuffer}
-            maxWidth={imageData?.imageRequest?.width}
+            maxWidth={imageWidth}
           />
         ))}
       </Carousel>
