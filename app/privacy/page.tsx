8e2f595024e97bf5c4575b043bdd29402f0d162d
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-07-26 12:51:52 -0700

    chore: add privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -5,11 +5,31 @@ export const metadata: Metadata = {
   title: 'Privacy Policy | ArtBot for Stable Diffusion'
 }
 
+async function getData() {
+  const res = await fetch('https://aihorde.net/api/v2/documents/privacy')
+  const data = res.json()
+
+  return data
+}
+
 export default async function PrivacyPage() {
+  const data = await getData()
+
+  data.html = data.html.replace('<h1>Privacy Policy</h1>\n', '')
+  data.html = data.html.replace(/<h1>/g, '<h1 class="font-bold text-lg">')
+  data.html = data.html.replace(
+    /<h2>/g,
+    '<h2 class="font-bold text-md italic">'
+  )
+  data.html = data.html.replace(/<h3>/g, '<h3 class="text-md italic">')
+
   return (
     <div>
       <PageTitle>Privacy Policy</PageTitle>
-      Placeholder for Privacy Policy
+      <div
+        className="col gap-4"
+        dangerouslySetInnerHTML={{ __html: data.html }}
+      ></div>
     </div>
   )
 }
