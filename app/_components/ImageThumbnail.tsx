226c99c9eeac516dbf38f0b90564e2cb9f6cee69
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-08-05 07:12:48 -0700

    refactor: update and fix rendering issue with image thumbnail

--- app/_components/ImageThumbnail.tsx
+++ app/_components/ImageThumbnail.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 /* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { checkImageExistsInDexie } from '../_db/ImageFiles'
 import { bufferToBlob } from '../_utils/imageUtils'
 import { ImageBlobBuffer } from '../_data-models/ImageFile_Dexie'
@@ -9,70 +9,62 @@ import { ImageBlobBuffer } from '../_data-models/ImageFile_Dexie'
 const defaultImage =
   'data:image/gif;base64,R0lGODdhAQABAJEAAAAAAB8fH////wAAACH5BAkAAAMALAAAAAABAAEAAAICTAEAOw=='
 
-// Memoized Image Component
-// Prevents re-rendering of the same image multiple times as parent is updated
-const ImageThumbnail = React.memo(
-  ({
-    alt,
-    artbot_id,
-    image_id
-  }: {
-    alt: string
-    artbot_id?: string
-    image_id?: string
-  }) => {
-    const [imageUrl, setImageUrl] = useState(defaultImage)
+const ImageThumbnail = ({
+  alt,
+  artbot_id,
+  image_id
+}: {
+  alt: string
+  artbot_id?: string
+  image_id?: string
+}) => {
+  // State to store the image URL, initialized with a default placeholder
+  const [imageUrl, setImageUrl] = useState(defaultImage)
 
-    useEffect(() => {
-      let isMounted = true // To prevent state update on unmounted component
+  // Ref to track whether the image has been loaded successfully
+  const imageLoaded = useRef(false)
 
-      const fetchImage = async () => {
-        let dexieImage
+  // useCallback to memoize the fetchImage function, preventing unnecessary re-renders
+  const fetchImage = useCallback(async () => {
+    // Check if the image has already been loaded to avoid redundant fetches
+    if (imageLoaded.current) return
 
-        if (artbot_id) {
-          dexieImage = await checkImageExistsInDexie({ artbot_id })
-        } else if (image_id) {
-          dexieImage = await checkImageExistsInDexie({ image_id })
-        }
+    let dexieImage
+    // Fetch the image from IndexedDB based on artbot_id or image_id
+    if (artbot_id) {
+      dexieImage = await checkImageExistsInDexie({ artbot_id })
+    } else if (image_id) {
+      dexieImage = await checkImageExistsInDexie({ image_id })
+    }
 
-        if (
-          dexieImage &&
-          dexieImage !== true &&
-          'imageBlobBuffer' in dexieImage &&
-          dexieImage.imageBlobBuffer &&
-          isMounted
-        ) {
-          if (!dexieImage.imageBlobBuffer) return
+    // If the image is found in IndexedDB, convert it to a blob and create a URL
+    if (
+      dexieImage &&
+      dexieImage !== true &&
+      'imageBlobBuffer' in dexieImage &&
+      dexieImage.imageBlobBuffer
+    ) {
+      const blob = bufferToBlob(dexieImage.imageBlobBuffer as ImageBlobBuffer)
+      const url = URL.createObjectURL(blob)
+      setImageUrl(url) // Update the image URL state
+      imageLoaded.current = true // Mark the image as loaded
+    }
+  }, [artbot_id, image_id])
 
-          const blob = bufferToBlob(
-            dexieImage.imageBlobBuffer as ImageBlobBuffer
-          )
-          const url = URL.createObjectURL(blob)
-          setImageUrl(url)
-        }
+  // useEffect to fetch the image when the component mounts and clean up URL object when unmounts
+  useEffect(() => {
+    fetchImage() // Call the fetchImage function when the component mounts
+
+    // Clean up the object URL when the component unmounts to avoid memory leaks
+    return () => {
+      if (imageUrl !== defaultImage) {
+        URL.revokeObjectURL(imageUrl)
       }
+    }
+  }, [fetchImage, imageUrl])
 
-      fetchImage()
+  // Render the <img> element with the retrieved or default image URL and the provided alt text
+  return <img alt={alt} src={imageUrl} />
+}
 
-      return () => {
-        isMounted = false // Clean up to prevent memory leaks
-
-        if (imageUrl !== defaultImage) {
-          URL.revokeObjectURL(imageUrl) // Clean up blob URL
-        }
-      }
-
-      // imageUrl in this dep array causes issue with URL.createObjectURL re-rendering thousands of times.
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [artbot_id])
-
-    return <img alt={alt} src={imageUrl} />
-  },
-  (prevProps, nextProps) => {
-    // Custom comparison function for React.memo
-    return prevProps.artbot_id === nextProps.artbot_id
-  }
-)
-
-ImageThumbnail.displayName = 'ImageThumbnail'
 export default ImageThumbnail
