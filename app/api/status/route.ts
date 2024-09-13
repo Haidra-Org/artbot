0824e6020e57ad55bd11c9a3943b330846ec99e1
Dave Schumaker <daveschumaker@users.noreply.github.com>
2024-09-12 20:00:47 -0700

    chore: attempt to fix deploy

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,14 +1,10 @@
-import 'dotenv/config';
 import { NextRequest } from 'next/server';
-
 const statusApi = process.env.ARTBOT_STATUS_API;
 
 export async function POST(request: NextRequest) {
   const { type } = await request.json();
 
   if (type === 'image_done' && statusApi) {
-    console.log(`DO WE GET HERE?!`);
-    console.log(`statusApi`, statusApi);
     fetch(`${statusApi}/status`, {
       method: 'POST',
       headers: {
@@ -21,7 +17,6 @@ export async function POST(request: NextRequest) {
 
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     }).catch((err) => {
-      console.log(`Errr sup?`, err);
       // Do nothing
     });
   }
--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
-import withSerwistInit from "@serwist/next";
-import withBundleAnalyzer from "@next/bundle-analyzer";
+import withSerwistInit from '@serwist/next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const withSerwist = withSerwistInit({
   cacheOnNavigation: true,
   reloadOnOnline: true,
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  swUrl: '/sw.js',
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+  swUrl: '/sw.js'
 });
 
 const BASE_PATH = process.env.BASE_PATH || ''; // Should be '' or '/artbot'
 const DEXIE_DB = process.env.DEXIE_DB || 'ArtBot_beta_v2';
-const HORDE_API_HOST = process.env.HORDE_API_HOST || 'https://aihorde.net'
+const HORDE_API_HOST = process.env.HORDE_API_HOST || 'https://aihorde.net';
 
 const nextConfig = {
   basePath: BASE_PATH,
@@ -20,14 +20,15 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_PATH: BASE_PATH,
     NEXT_HORDE_API_HOST: HORDE_API_HOST,
     NEXT_PUBLIC_DEXIE_DB: DEXIE_DB,
-    NEXT_PUBLIC_SAVE_DEBUG_LOGS: "false", // "true" or "false"
-    NEXT_TELEMETRY_DISABLED: "1" // disable Vercel / NextJS telemetry
+    NEXT_PUBLIC_SAVE_DEBUG_LOGS: 'false', // "true" or "false"
+    NEXT_TELEMETRY_DISABLED: '1', // disable Vercel / NextJS telemetry
+    ARTBOT_STATUS_API: process.env.ARTBOT_STATUS_API
   },
   output: 'standalone'
-}
+};
 
 const analyzeBundleConfig = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
+  enabled: process.env.ANALYZE === 'true'
+});
 
 export default analyzeBundleConfig(withSerwist(nextConfig));
